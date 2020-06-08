e3e10b30725fcf3bd216f7a997e460df5b3078bc
Matthias Berth <matthias.berth@gmail.com>
2020-06-08 21:58:57 +0200

    Start testting

---
+++ tests/html_examples/README.md
@@ -0,0 +1,2 @@
+- California_wikipedia_page_2020-04-25.html has complex contents in the "Recovered" field (`4481 ([Johns Hopkins University`).
+  The original page is at https://en.wikipedia.org/w/index.php?title=COVID-19_pandemic_in_California&oldid=953061844.
--- tests/test_scrape_recoveries.py
+++ tests/test_scrape_recoveries.py
@@ -1,2 +1,11 @@
+from ecv_analytics.scrape_recoveries import extract_recovered
+import importlib.resources as pkg_resources
+
 def test_dummy():
     assert 2 + 2 == 4
+
+    from . import \
+        html_examples  # relative-import the *package* containing the templates
+    #
+    page = pkg_resources.read_text(html_examples, 'California_wikipedia_page_2020-04-25.html')
+    assert "Recovered" in page
