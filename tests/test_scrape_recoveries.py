019ff06db8757f62c2c746caa9bbe3fd879c148d
Matthias Berth <matthias.berth@gmail.com>
2020-06-08 22:04:52 +0200

    cleanup test

--- tests/test_scrape_recoveries.py
+++ tests/test_scrape_recoveries.py
@@ -1,11 +1,11 @@
-from ecv_analytics.scrape_recoveries import extract_recovered
+from ecv_analytics.scrape_recoveries import extract_recovered_from_html
 import importlib.resources as pkg_resources
+from . import html_examples
 
-def test_dummy():
-    assert 2 + 2 == 4
-
-    from . import \
-        html_examples  # relative-import the *package* containing the templates
-    #
+def test_recovered_extra_text():
+    """Parse this correctly:
+        Recovered	4481 ([Johns Hopkins University
+    """
     page = pkg_resources.read_text(html_examples, 'California_wikipedia_page_2020-04-25.html')
-    assert "Recovered" in page
+    assert extract_recovered_from_html(page) == 4481
+
